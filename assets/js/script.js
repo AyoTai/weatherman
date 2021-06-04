cb1025252d68dafb884ae26577114e797af93c3d
Tai Mujarin <tai.mujarin@gmail.com>
2021-06-03 23:36:29 -0700

    made button console log data

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,45 @@ const resultContentEl = document.querySelector('#result-content');
 const searchBtn = document.querySelector('.btn');
 const searchInput = document.querySelector('#search-input');
 // const fiveDayForcast = document.querySelector('#5dayforcast');
-const searchInputVal = document.querySelector('#search-input').value;
+var searchInputVal = document.querySelector('#search-input').value;
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
-
 const apiKey = '&appid=5d050cc05f2e639349f41af0545c2086';
+const button = document.querySelector('.btn')
 
 // const currentDay = moment().format("MM-DD-YYYY");
 
 
 // Functions //
+function searchWeather(input){
+    
+    url = baseUrl + searchInputVal + apiKey;
+
+    fetch(url)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            // console.log(data);
+            // for (let i = 0; i < data.length; i++) {
+            console.log(data.name)
+            console.log(data.weather[0].icon)
+            console.log(data.main.temp)
+            console.log(data.wind.speed)
+            console.log(data.main.humidity)
+            console.log(data.main.uvi)    
+            
+        })
+}
+
 function handleSearchFormSubmit(event){
     event.preventDefault();
 
+    searchInputVal = document.querySelector('#search-input').value;
+
     if (!searchInputVal){
-        alert("You Need to Enter a Valid Name");
+        alert("You Need to Enter a Valid Name!");
     }
+    searchWeather(searchInputVal);
 }
 
-class Fetch{
-    async getCurrent() {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=california&appid=5d050cc05f2e639349f41af0545c2086`);
-
-        const data = await response.json();
-
-        console.log(data);
-
-        return data;
-    }
-}
+button.addEventListener('click', handleSearchFormSubmit);

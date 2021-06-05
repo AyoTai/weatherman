c539ae869ff533a9fa6e519fe0a0f33a405466d7
Tai Mujarin <tai.mujarin@gmail.com>
2021-06-04 23:03:15 -0700

    added 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,13 @@ const searchCityEl = document.querySelector('#search-city');
 const resultContentEl = document.querySelector('#result-content');
 const searchBtn = document.querySelector('.btn');
 const searchInput = document.querySelector('#search-input');
-// const fiveDayForcast = document.querySelector('#5dayforcast');
+const fiveDayForcast = document.querySelector('#fivedayforecast');
 var searchInputVal = document.querySelector('#search-input').value;
 const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='
-const apiKey = '&units=imperial&appid=c8659540ec6ffab5c1dacd3f595ebb2e';
+const apiKey = '&exclude=hourly,minutely,alerts&appid=c8659540ec6ffab5c1dacd3f595ebb2e&units=imperial';
 const button = document.querySelector('.btn')
 const weathers = [];
+var forecastDay = [];
 var currentDay = moment().format("MM-DD-YYYY");
 
 
@@ -60,39 +61,41 @@ function searchWeather(lattitude, longitude){
             }
             weathers.push(weather);
             displayWeather(data);
+            displayForecast(data);
         })
         searchInputVal = '';
 }
 
+// to display the weather 
 function displayWeather(data){
     var weathResultDiv = document.createElement('div');
     weathResultDiv.className = 'weather-result float-child';
 
     var weathCity = document.createElement('h4');
     weathCity.textContent = data.timezone + ' ' + currentDay;
-    weathCity.className = 'weather-city';
+    weathCity.className = 'weather';
 
     var weathIcon = document.createElement('img');
     weathIcon.src = "https://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png";
-    weathIcon.className = 'weather-icon';
+    weathIcon.className = 'weather';
 
     var weathTemp = document.createElement('p');
     weathTemp.textContent = "Temp: " + data.current.temp + " °F";
-    weathTemp.className = 'weather-temp';
+    weathTemp.className = 'weather';
 
     var weathWind = document.createElement('p');
     weathWind.textContent = "Wind Speed: " + data.current.wind_speed + " MPH";
-    weathWind.className = 'weather-windspeed';
+    weathWind.className = 'weather';
 
     var weathHum = document.createElement('p');
-    weathHum.textContent = "Humidity: " + data.current.humidity + " %";
-    weathHum.className = 'weather-humidity';
+    weathHum.textContent = "Humidity: " + data.current.humidity + "%";
+    weathHum.className = 'weather';
 
     var weathUvi = document.createElement('p');
     weathUvi.textContent = "UV Index: ";
-    weathUvi.className = 'weather-uvi';
+    weathUvi.className = 'weather';
     var uviSpan = document.createElement('span');
-    uviSpan.className = 'badge';
+    uviSpan.className = 'badge weather';
     uviSpan.textContent = data.current.uvi;
     if (data.current.uvi < 5){
         uviSpan.classList.add("badge1");
@@ -113,17 +116,66 @@ function displayWeather(data){
     document.querySelector('.result-content').appendChild(weathResultDiv);
 }
 
-function clearDiv(){
-    document.querySelector('.weather-result').remove();
+// to display 5day forecast
+function displayForecast(data){
+    var forecastHeader = document.createElement('h3');
+    forecastHeader.textContent = "5 Day Forecast";
+    forecastHeader.className = "text-center forecast";
+    fiveDayForcast.appendChild(forecastHeader);
+    fiveDays();
+
+    for (let i = 0; i < 5; i++){
+        var weathCards = document.createElement('div');
+        weathCards.className = "col-2 col-md-4 col-sm-6 float-child cards";
+        weathCards.textContent = forecastDay[i];
+        console.log(forecastDay[i]);
+        fiveDayForcast.appendChild(weathCards);
+        
+        var cardIcon = document.createElement('img');
+        cardIcon.className = 'weather';
+        cardIcon.src = "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png";
+        weathCards.appendChild(cardIcon);
+
+        var cardTemp = document.createElement('p');
+        cardTemp.className = 'weather';
+        cardTemp.textContent = "Temp: " + data.daily[i].temp.day + "°F";
+        weathCards.appendChild(cardTemp);
+
+        var cardHum = document.createElement('p');
+        cardHum.className = 'weather';
+        cardHum.textContent = "Humidity: " + data.daily[i].humidity + "%";
+        weathCards.appendChild(cardHum);
+
+        var cardWind = document.createElement('p');
+        cardWind.className = 'weather';
+        cardWind.textContent = "Wind Speed: " + data.daily[i].wind_speed + " MPH";
+        weathCards.appendChild(cardWind);
+    }
 }
 
+// 5 day forcast
+function fiveDays() {
+    for (var i = 1; i < 6; i++) {
+      var newDay = moment().add(i, "days").format("MM-DD-YYYY");
+      forecastDay.push(newDay);
+    }
+  }
+
+
+// to clear last result before appending more
+function clearDiv(){
+    document.querySelectorAll('.weather').remove();
+}
+
+
+// to search user input
 function handleSearchFormSubmit(event){
     event.preventDefault();
 
     searchInputVal = document.querySelector('#search-input').value;
 
     if (!searchInputVal){
-        alert("You Need to Enter a Valid Name!");
+        alert("You Need to Enter a Valid City!");
     }
     geoCoords();
     clearDiv();
